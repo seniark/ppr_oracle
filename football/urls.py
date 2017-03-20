2e42eec92b8cbdb9d96cfbb6488de24395d92331
Kevin Allison <kvnallsn@gmail.com>
2017-03-20 19:14:11 -0400

    Auth login/Team view
    Added login and reworked the team view to use smaller logos

--- football/urls.py
+++ football/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+
         url(r'^teams/$',  views.TeamListView.as_view(), name='teams'),
+        url(r'^teams/(?P<pk>[A-Z]{2,3})/$', views.TeamDetailView.as_view(), name='team-detail'),
+
         url(r'^players/$', views.PlayerListView.as_view(), name='players'),
 
         url(r'^players/quarterbacks/$', views.players_quarterbacks, name='pos-qb'),
